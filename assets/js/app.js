87c414d205ca3cd62753606f98b99cfed48a04d6
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2020-10-20 22:53:32 +0530

    heroku setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,12 @@
 const cardBtn = document.getElementsByClassName('card-wrapper');
 const popCardModal = document.getElementById("pop-card-modal")
 const cancelBtn = document.getElementById('pop-card-cancel');
+
 function showCardModal() {
     popCardModal.style.display = 'flex';
     popCardModal.style.animation = 'pop-card 0.5s ease-out forwards';
 }
+
 function hideCardModal() {
     popCardModal.style.display ='none';
 }
